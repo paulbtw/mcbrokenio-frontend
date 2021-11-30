8bbd7ce035a2bd7bd130d361ed73a90b6602d100
Paulbtw <p-p-v-2010@hotmail.de>
2021-11-30 19:48:20 +0100

    feat: add broken since

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,20 +1,37 @@
 import React from 'react';
-import { Flex, Text, Badge, Box } from '@chakra-ui/react';
+import { Flex, Text, Badge, Box, Tooltip } from '@chakra-ui/react';
+import { formatDistance } from 'date-fns';
 import { Availability } from '../types/types';
 
 interface BadgeProps {
   name: string;
   hasItem: Availability;
+  brokenSince: number | null;
 }
 
-export const Status: React.FC<BadgeProps> = ({ name, hasItem }) => {
+export const Status: React.FC<BadgeProps> = ({
+  name,
+  hasItem,
+  brokenSince,
+}) => {
   if (hasItem === Availability.NOT_APPLICABLE) return null;
   const hasItemRender = (active: Availability) => {
     if (active === Availability.AVAILABLE) {
       return <Badge colorScheme="green">Available</Badge>;
     }
     if (active === Availability.NOT_AVAILABLE) {
-      return <Badge colorScheme="red">Unavailable</Badge>;
+      return (
+        <Tooltip
+          label={
+            brokenSince
+              ? `for ${formatDistance(brokenSince, new Date())}`
+              : undefined
+          }
+          placement="top"
+        >
+          <Badge colorScheme="red">Unavailable</Badge>
+        </Tooltip>
+      );
     }
     return <Badge colorScheme="gray">Unknown</Badge>;
   };
