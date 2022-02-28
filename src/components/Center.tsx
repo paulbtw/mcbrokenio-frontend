8af76122ac9b19953977ead923d463f2d697351d
paulbtw <paul.vatiche@gmail.com>
2022-02-28 15:12:04 +0100

    fix: currentLocation

--- src/components/Center.tsx
+++ src/components/Center.tsx
@@ -14,7 +14,7 @@ interface IProps {
   currentLocation: {
     lat: number;
     lon: number;
-  };
+  } | null;
   countryStats: ICountryStats[] | undefined;
 }
 
@@ -38,7 +38,7 @@ export const Center: FC<IProps> = ({
         </TabList>
         <TabPanels width="100%" height="100%">
           <TabPanel width="100%" height="100%" p={0}>
-            {markers && (
+            {markers && currentLocation && (
               <Map markers={markers} currentLocation={currentLocation} />
             )}
           </TabPanel>
